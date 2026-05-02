11fa769d2975eb75b1c3112e2752616777464574
Sávio Macêdo <saviosousa079@gmail.com>
2026-05-02 00:40:08 -0300

    estabilizar o re run

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,36 +1,41 @@
 from selenium.webdriver.common.by import By
+
 from pages.base_page import BasePage
-import time
+
 
 class CartPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self._add_to_cart_button = (By.ID, "add-to-cart-sauce-labs-backpack")
-        self._cart_icon = (By.CLASS_NAME, "shopping_cart_link")
-        self._inventory_item_name = (By.CLASS_NAME, "inventory_item_name")
-        self._checkout_button = (By.ID, "checkout")
+        self._add_to_cart_button = (
+            By.CSS_SELECTOR,
+            '[data-test="add-to-cart-sauce-labs-backpack"]',
+        )
+        self._cart_badge = (By.CSS_SELECTOR, '[data-test="shopping-cart-badge"]')
+        self._cart_icon = (By.CSS_SELECTOR, '[data-test="shopping-cart-link"]')
+        self._inventory_item_name = (
+            By.CSS_SELECTOR,
+            '[data-test="inventory-item-name"]',
+        )
+        self._checkout_button = (By.CSS_SELECTOR, '[data-test="checkout"]')
+
+    def _click_now(self, locator):
+        element = self.driver.find_element(*locator)
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView({block: 'center', inline: 'center'});",
+            element,
+        )
+        self.driver.execute_script("arguments[0].click();", element)
 
     def adicionar_produto_ao_carrinho(self):
-        self.click(self._add_to_cart_button)
+        self._click_now(self._add_to_cart_button)
+        self.wait_visible(self._cart_badge)
 
     def acessar_carrinho(self):
-        self.click(self._cart_icon)
+        self._click_now(self._cart_icon)
         self.wait_url_contains("cart.html")
 
     def iniciar_checkout(self):
-        btn = self.wait_clickable(self._checkout_button)
-        for _ in range(3):
-            try:
-                btn.click()
-               
-                self.wait_url_contains("checkout-step-one.html")
-                return 
-            except Exception:
-        
-                self.driver.execute_script("arguments[0].click();", btn)
-                time.sleep(1) 
-        
-    
+        self._click_now(self._checkout_button)
         self.wait_url_contains("checkout-step-one.html")
 
     def nome_produto_no_carrinho(self) -> str:
