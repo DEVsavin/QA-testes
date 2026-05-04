29e679356c5f46edfa62819fae098c8d5eac3993
Sávio Macêdo <saviosousa079@gmail.com>
2026-05-04 19:36:59 -0300

    FIX E2E

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -18,24 +18,16 @@ class CartPage(BasePage):
         )
         self._checkout_button = (By.CSS_SELECTOR, '[data-test="checkout"]')
 
-    def _click_now(self, locator):
-        element = self.driver.find_element(*locator)
-        self.driver.execute_script(
-            "arguments[0].scrollIntoView({block: 'center', inline: 'center'});",
-            element,
-        )
-        self.driver.execute_script("arguments[0].click();", element)
-
     def adicionar_produto_ao_carrinho(self):
-        self._click_now(self._add_to_cart_button)
+        self.click(self._add_to_cart_button)
         self.wait_visible(self._cart_badge)
 
     def acessar_carrinho(self):
-        self._click_now(self._cart_icon)
+        self.click(self._cart_icon)
         self.wait_url_contains("cart.html")
 
     def iniciar_checkout(self):
-        self._click_now(self._checkout_button)
+        self.click(self._checkout_button)
         self.wait_url_contains("checkout-step-one.html")
 
     def nome_produto_no_carrinho(self) -> str:
